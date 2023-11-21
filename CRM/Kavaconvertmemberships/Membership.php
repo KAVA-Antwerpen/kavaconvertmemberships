6d9d352b971f78914e7222d8c7dc6a1c7970184a
Alain Benbassat <alain@businessandcode.eu>
2023-11-21 11:54:05 +0100

    fix

--- CRM/Kavaconvertmemberships/Membership.php
+++ CRM/Kavaconvertmemberships/Membership.php
@@ -168,6 +168,47 @@ class CRM_Kavaconvertmemberships_Membership {
     }
   }
 
+  public static function convertCorresponderendLid($contactId = NULL) {
+    $currentYear = (int)date('Y');
+    $nextYear = $currentYear + 1;
+
+    // corresponderende leden
+    $memberships = \Civi\Api4\Membership::get(FALSE)
+      ->addSelect('*', 'custom.*')
+      ->addWhere('membership_type_id', '=', CRM_Kavaconvertmemberships_MembershipType::CORRESPONDERENDE_LEDEN)
+      ->addWhere('end_date', '>', "$currentYear-12-31");
+
+    if ($contactId) {
+      $memberships = $memberships->addWhere('contact_id', '=', $contactId);
+    }
+
+    $memberships = $memberships->execute();
+
+    // eindig op einde van het jaar en creeer nieuw lidm.
+    foreach ($memberships as $membership) {
+      \Civi\Api4\Membership::update(FALSE)
+        ->addValue('end_date', "$currentYear-12-31")
+        ->addWhere('id', '=', $membership['id'])
+        ->execute();
+
+      $m = \Civi\Api4\Membership::create(FALSE)
+        ->addValue('contact_id', $membership['contact_id'])
+        ->addValue('membership_type_id', CRM_Kavaconvertmemberships_MembershipType::get('KAVA werkelijk lid'))
+        ->addValue('status_id', CRM_Kavaconvertmemberships_MembershipStatus::get('Actief'))
+        ->addValue('join_date', $membership['join_date'])
+        ->addValue('start_date', "$nextYear-01-01")
+        ->addValue('end_date', '3000-01-01')
+        ->addValue('is_override', TRUE)
+        ->addValue('Facturatie.Betaler', $membership['Facturatie.Betaler'])
+        ->addValue('Facturatie.Gratis_', $membership['Facturatie.Gratis_'])
+        ->addValue('Facturatie.Product', '&BV500020')
+        ->execute();
+
+      // reset is override
+      CRM_Core_DAO::executeQuery("update civicrm_membership set is_override = 0 where id = " . $m[0]['id']);
+    }
+  }
+
   private static function getRelationshipsWithType($relType, $year, $isActive, $contactId = NULL) {
     // afgestudeerden op basis van relatie ophalen
     $relationships = \Civi\Api4\Relationship::get(FALSE)
